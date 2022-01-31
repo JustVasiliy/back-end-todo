0da5435bbb3f1850c4708576d14f1004d1d5df3c
Vasiliy <vasilyrd2001@gmail.com>
2022-01-31 09:39:02 +0200

    fixed littke bug with authorization

--- service /catchError.js
+++ service /catchError.js
@@ -2,6 +2,7 @@ module.exports = async function (ctx, next) {
   try {
     await next();
   } catch (err) {
+    
     ctx.status = err.statusCode || err.status || 500;
     ctx.body = JSON.stringify({
       message: "error",
--- service /jwt.js
+++ service /jwt.js
@@ -1,28 +1,25 @@
-const koaJwt = require("koa-jwt");
 const jwt = require("jsonwebtoken");
 const User = require("../schemas/userShema");
-const invalidError = require('./invalidError')
+const invalidError = require("./invalidError");
 module.exports = async function jwtVerifucation(ctx, next) {
   const token = ctx.request.headers.authorization;
   const decodeToken = jwt.decode(token);
-  const test = jwt.verify(token, 'myKey', function(err){
-    if(err.name === 'TokenExpiredError'){
-      return 'TokenExpiredError'
+  const jwtVerify = jwt.verify(token, "myKey", function (err) {
+    if (err !== null) {
+      return "TokenExpiredError";
     }
-    
-  })
-  
+  });
+
   if (decodeToken !== null) {
     const userFind = await User.User.find({ id: decodeToken.id });
-    if (
-      userFind[0] !== undefined &&
-      test !== 'TokenExpiredError'
-    ) {
+
+    if (userFind[0] !== undefined && jwtVerify !== "TokenExpiredError") {
       return next();
     } else {
-     invalidError.invalidError(ctx)
+      console.log("here");
+      invalidError.invalidError(ctx);
     }
   } else {
-    invalidError.invalidError(ctx)
+    invalidError.invalidError(ctx);
   }
 };
