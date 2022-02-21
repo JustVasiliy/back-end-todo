933f5e141341ae8846870f7351f138a2ba488de8
Vasiliy <vasilyrd2001@gmail.com>
2022-02-21 11:09:33 +0200

    creat four tests, but they don't work

--- test/endpoint.test.js
+++ test/endpoint.test.js
@@ -1,22 +1,63 @@
 import request from "supertest";
-const baseUrl = 'http://localhost:3000';
-const app = require('../server/server')
+const baseUrl = "http://localhost:3000";
+const app = require("../server/server");
 const server = app.listen(3000);
+const jwt = require("jsonwebtoken");
+describe("Todos endpoint", () => {
+  // jest.setTimeout(15000)
+  const token = jwt.sign({ nickname: "uio", id: "e93f9316-6430-4d0e-99b5-cb71eaedd281" }, "myKey", {
+    expiresIn: 60 * 60,
+  });
+  it("GET", async () => {
+    const response = await request(baseUrl)
+      .get("/api/task/get")
+      .set("Authorization", token);
 
-describe('Todos endpoint', () => {
- 
-	it('should return a 200 status code', async () => {
-		const response = await request(baseUrl)
-			.get('/api/task/get');
-   
-		expect(response.statusCode).toBe(401);
-    
-    
-    
-	});
-	afterAll(()=>{
-		server.close()
-	})
-  
-})
+    expect(response.statusCode).toBe(200);
+  });
+  it("POST", async () => {
+    const response = await request(baseUrl)
+      .post("/api/task/create")
+      .set("Authorization", token)
+      .send({
+        name: "test",
+        checked: false,
+        deleted: false,
+        id: "test",
+        createdBy: "test",
+      });
 
+    expect(response.statusCode).toBe(200);
+  });
+  it("PUT", async () => {
+    const response = await request(baseUrl)
+      .post("/api/task/put")
+      .set("Authorization", token)
+      .send({
+        name: "test",
+        checked: false,
+        deleted: false,
+        id: "test",
+        createdBy: "test",
+      });
+
+    expect(response.statusCode).toBe(200);
+  });
+  it("DELETE", async () => {
+    const response = await request(baseUrl)
+      .post("/api/task/delete")
+      .set("Authorization", token)
+      .send({
+        name: "test",
+        checked: false,
+        deleted: false,
+        id: "test",
+        createdBy: "test",
+      });
+
+    expect(response.statusCode).toBe(200);
+  });
+  afterAll(() => {
+    server.close();
+  });
+});
